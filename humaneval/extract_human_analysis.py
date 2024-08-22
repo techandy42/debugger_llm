963efaa5eba217143348a1df4ecfadaeef63995d
Hokyung (Andy) Lee <techandy42@gmail.com>
2024-08-22 07:29:32 -0400

    extracted human buggy code analysis

---
+++ humaneval/extract_human_analysis.py
@@ -0,0 +1,44 @@
+import os
+import glob
+import pandas as pd
+from helpers import file_exists
+
+def get_custom_txt_files(directory):
+    search_pattern = os.path.join(directory, '*_custom.txt')
+    custom_files = glob.glob(search_pattern)
+    custom_file_names = [os.path.basename(file) for file in custom_files]
+    return custom_file_names
+
+def extract_text_from_file(file_path):
+    with open(file_path, 'r') as file:
+        lines = file.readlines()
+
+    if not lines:
+        raise ValueError("The file is empty or does not exist.")
+
+    first_line = lines[0]
+    task_id = first_line.split("TASK ID: ")[1].strip() if "TASK ID: " in first_line else None
+
+    remaining_lines = lines[3:]
+
+    if remaining_lines and remaining_lines[-1].strip() == "":
+        remaining_lines = remaining_lines[:-1]
+
+    remaining_text = '\n'.join([line.strip() for line in remaining_lines])
+
+    return task_id, remaining_text
+
+DATASET_PATH = "humaneval_dataset.csv"
+
+if file_exists(DATASET_PATH):
+  humaneval_train_df = pd.read_csv(DATASET_PATH)
+else:
+  raise FileNotFoundError(f"File {DATASET_PATH} not found")
+
+custom_analysis_filenames = get_custom_txt_files('humaneval_custom_dataset')
+for filename in custom_analysis_filenames:
+    file_path = os.path.join('humaneval_custom_dataset', filename)
+    task_id, content = extract_text_from_file(file_path)
+    humaneval_train_df.loc[humaneval_train_df['task_id'] == task_id, 'analysis_custom'] = content
+
+humaneval_train_df.to_csv(DATASET_PATH, index=False)
