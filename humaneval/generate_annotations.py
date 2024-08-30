e0845759e3fb2de3cd80717123b4fc9c68e14190
Hokyung (Andy) Lee <techandy42@gmail.com>
2024-08-29 22:59:38 -0500

    annotated 1st test dataset problem

--- humaneval/generate_annotations.py
+++ humaneval/generate_annotations.py
@@ -2,8 +2,8 @@ import random
 import pandas as pd
 from helpers import file_exists, indent_lines, force_delete_directory, create_directories, save_string_to_file
 
-DATASET_PATH = "humaneval_dataset.csv"
-STORAGE_PATH = "analysis_claude-3-5-sonnet-20240620_human_annotation"
+DATASET_PATH = "humaneval_test_dataset.csv"
+STORAGE_PATH = "analysis_claude-3-5-sonnet-20240620_human_annotation_test_dataset"
 MODEL_NAME = "claude-3-5-sonnet-20240620"
 JUDGE_MODEL_NAME = "claude-3-5-sonnet-20240620"
 JUDGE_ID = "cl35s-v1"
